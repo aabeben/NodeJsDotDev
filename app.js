777bf65ee3a427a067eaf39874b1c980be869133
Beny Hasanudin <bhasanudin@gmail.com>
2020-06-18 12:43:51 +0700

    Print only one apples and oranges

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const x = 1
-const y = 2
-const z = 3
+const oranges = ['oranges', 'orange']
+const apples = ['just one apple']
 
-console.count(
-  'The value of x is ' + x +
-  ' and has been checked .. how many times?'
-)
-console.count(
-  'The value of x is ' + x +
-  ' and has been checked .. how many times?'
-)
-console.count(
-  'The value of y is ' + y +
-  ' and has been checked .. how many times?'
-)
+oranges.forEach( fruit => {
+	console.count(fruit)
+})
+apples.forEach( fruit => {
+  console.count(fruit)
+})
