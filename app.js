959aaed9962661c3c76a3318b91351bd8c5e2e92
Beny Hasanudin <bhasanudin@gmail.com>
2020-06-18 22:13:05 +0700

    Menggunakan console.trace untuk melihat jejak tumpukan fungsi

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const args = require('minimist')(process.argv.slice(2))
-console.log(args['name'])
+// Print the stack trace
+// there might be cases where it's useful to print the call stack trace of a function
+// , maybe to answer the question how did you reach that part of the code?
+const func2 = ()=> console.trace()
+const func1 = ()=>func2()
+func1()
