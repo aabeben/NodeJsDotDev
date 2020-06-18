c797dc95e3329c03013ff9ad41aec7d6ffa41b6d
Beny Hasanudin <bhasanudin@gmail.com>
2020-06-18 11:52:12 +0700

    Using many console format

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const x = 'x', y='y'
-console.log(x, y)
+const name = 'Beny Hasanudin'
+const age = 36
+const weight = 58.9
 
-console.log('My %s has %d years', 'cat', 2)
+const beny = {
+    name:name,
+    age:age,
+    weight:weight,
+    address: 'Harvest City Cluster Olive'
+}
+console.log('Hello my name is: %s, %d years old, weight %f', name, age, weight)
+console.log('beny object: %O',beny)
+console.log('%O', Number)
